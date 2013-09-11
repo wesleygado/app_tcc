a342efe91cd82f236e38be8bdbc23d9daa90529c
wesleygado <wesleygado@gmail.com>
2013-09-10 22:03:36 -0300

    PRIMEIRO COMMIT
    PRIMEIRO COMMIT COM LAYOUT - TABS

---
+++ tcc/HunterFood/src/com/hunters/hunterfood/OndeEstouActivity.java
@@ -0,0 +1,22 @@
+package com.hunters.hunterfood;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class OndeEstouActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_onde_estou);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.onde_estou, menu);
+		return true;
+	}
+
+}
